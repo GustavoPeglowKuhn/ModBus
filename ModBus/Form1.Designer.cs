51c6e17320367ad8996706584b2fb5ec7184ba64
GustavoKuhn <gustavowolgep@gmail.com>
2017-03-05 17:59:24 +0000

    Almost a serial terminal
    falta colocar o batao de enviar e o timer para receber os dados

--- ModBus/Form1.Designer.cs
+++ ModBus/Form1.Designer.cs
@@ -23,12 +23,147 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components=new System.ComponentModel.Container();
-			this.AutoScaleMode=System.Windows.Forms.AutoScaleMode.Font;
-			this.Text="Form1";
+			this.components = new System.ComponentModel.Container();
+			this.serialPort = new System.IO.Ports.SerialPort(this.components);
+			this.tb_send = new System.Windows.Forms.TextBox();
+			this.tb_receive = new System.Windows.Forms.TextBox();
+			this.btn_send = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.ms_SerialPort = new System.Windows.Forms.ToolStripMenuItem();
+			this.ms_sp_port = new System.Windows.Forms.ToolStripMenuItem();
+			this.ms_sp_baud = new System.Windows.Forms.ToolStripMenuItem();
+			this.ms_sp_baud_combobox = new System.Windows.Forms.ToolStripComboBox();
+			this.ms_sp_port_combobox = new System.Windows.Forms.ToolStripComboBox();
+			this.ms_sp_conect = new System.Windows.Forms.ToolStripMenuItem();
+			this.ms_sp_disconect = new System.Windows.Forms.ToolStripMenuItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tb_send
+			// 
+			this.tb_send.Location = new System.Drawing.Point(12, 27);
+			this.tb_send.Name = "tb_send";
+			this.tb_send.Size = new System.Drawing.Size(443, 20);
+			this.tb_send.TabIndex = 0;
+			// 
+			// tb_receive
+			// 
+			this.tb_receive.Location = new System.Drawing.Point(12, 82);
+			this.tb_receive.Multiline = true;
+			this.tb_receive.Name = "tb_receive";
+			this.tb_receive.Size = new System.Drawing.Size(443, 107);
+			this.tb_receive.TabIndex = 1;
+			// 
+			// btn_send
+			// 
+			this.btn_send.Location = new System.Drawing.Point(12, 53);
+			this.btn_send.Name = "btn_send";
+			this.btn_send.Size = new System.Drawing.Size(443, 23);
+			this.btn_send.TabIndex = 2;
+			this.btn_send.Text = "Send";
+			this.btn_send.UseVisualStyleBackColor = true;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ms_SerialPort});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(467, 24);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// ms_SerialPort
+			// 
+			this.ms_SerialPort.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ms_sp_port,
+            this.ms_sp_baud,
+            this.ms_sp_conect,
+            this.ms_sp_disconect});
+			this.ms_SerialPort.Name = "ms_SerialPort";
+			this.ms_SerialPort.Size = new System.Drawing.Size(69, 20);
+			this.ms_SerialPort.Text = "SerialPort";
+			// 
+			// ms_sp_port
+			// 
+			this.ms_sp_port.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ms_sp_port_combobox});
+			this.ms_sp_port.Name = "ms_sp_port";
+			this.ms_sp_port.Size = new System.Drawing.Size(152, 22);
+			this.ms_sp_port.Text = "Port";
+			// 
+			// ms_sp_baud
+			// 
+			this.ms_sp_baud.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ms_sp_baud_combobox});
+			this.ms_sp_baud.Name = "ms_sp_baud";
+			this.ms_sp_baud.Size = new System.Drawing.Size(152, 22);
+			this.ms_sp_baud.Text = "BaudRate";
+			// 
+			// ms_sp_baud_combobox
+			// 
+			this.ms_sp_baud_combobox.Name = "ms_sp_baud_combobox";
+			this.ms_sp_baud_combobox.Size = new System.Drawing.Size(121, 23);
+			this.ms_sp_baud_combobox.SelectedIndexChanged += new System.EventHandler(this.ms_sp_baud_combobox_SelectedIndexChanged);
+			// 
+			// ms_sp_port_combobox
+			// 
+			this.ms_sp_port_combobox.Name = "ms_sp_port_combobox";
+			this.ms_sp_port_combobox.Size = new System.Drawing.Size(121, 23);
+			this.ms_sp_port_combobox.DropDown += new System.EventHandler(this.ms_sp_port_combobox_DropDown);
+			this.ms_sp_port_combobox.SelectedIndexChanged += new System.EventHandler(this.ms_sp_port_combobox_SelectedIndexChanged);
+			// 
+			// ms_sp_conect
+			// 
+			this.ms_sp_conect.Name = "ms_sp_conect";
+			this.ms_sp_conect.Size = new System.Drawing.Size(152, 22);
+			this.ms_sp_conect.Text = "Conect";
+			this.ms_sp_conect.Click += new System.EventHandler(this.ms_sp_conect_Click);
+			// 
+			// ms_sp_disconect
+			// 
+			this.ms_sp_disconect.Enabled = false;
+			this.ms_sp_disconect.Name = "ms_sp_disconect";
+			this.ms_sp_disconect.Size = new System.Drawing.Size(152, 22);
+			this.ms_sp_disconect.Text = "Disconect";
+			this.ms_sp_disconect.Click += new System.EventHandler(this.ms_sp_disconect_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(467, 281);
+			this.Controls.Add(this.btn_send);
+			this.Controls.Add(this.tb_receive);
+			this.Controls.Add(this.tb_send);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.IO.Ports.SerialPort serialPort;
+		private System.Windows.Forms.TextBox tb_send;
+		private System.Windows.Forms.TextBox tb_receive;
+		private System.Windows.Forms.Button btn_send;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem ms_SerialPort;
+		private System.Windows.Forms.ToolStripMenuItem ms_sp_port;
+		private System.Windows.Forms.ToolStripMenuItem ms_sp_baud;
+		private System.Windows.Forms.ToolStripComboBox ms_sp_baud_combobox;
+		private System.Windows.Forms.ToolStripComboBox ms_sp_port_combobox;
+		private System.Windows.Forms.ToolStripMenuItem ms_sp_conect;
+		private System.Windows.Forms.ToolStripMenuItem ms_sp_disconect;
+		private System.Windows.Forms.Timer timer1;
 	}
 }
 
--- ModBus/Form1.cs
+++ ModBus/Form1.cs
@@ -10,8 +10,61 @@ using System.Windows.Forms;
 
 namespace ModBus {
 	public partial class Form1 : Form {
+
+		//enum BaudRate { asd, assd};
+		static int[] iBaudRate = { 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000 };
+		static string[] sBaudRate = { "9600", "14400", "19200", "38400", "57600", "115200", "128000", "256000" };
+		
+
 		public Form1() {
 			InitializeComponent();
+
+			ms_sp_baud_combobox.Items.AddRange(sBaudRate);
+		}
+
+		private void ms_sp_port_combobox_DropDown(object sender, EventArgs e) {
+			ms_sp_port_combobox.Items.Clear();
+			foreach(string port in System.IO.Ports.SerialPort.GetPortNames()) {
+				ms_sp_port_combobox.Items.Add(port);
+			}
+		}
+
+		private void ms_sp_port_combobox_SelectedIndexChanged(object sender, EventArgs e) {
+			if(ms_sp_port_combobox.Items.Count==0) {
+				MessageBox.Show("Conect any serial device!", "No serial ports found");
+			}
+			serialPort.PortName=ms_sp_port_combobox.SelectedItem.ToString();
+
+			tb_receive.Text="Debug - "+serialPort.PortName;
+		}
+
+		private void ms_sp_baud_combobox_SelectedIndexChanged(object sender, EventArgs e) {
+			serialPort.BaudRate = iBaudRate[ms_sp_baud_combobox.SelectedIndex];
+			//tb_receive.Text="Debug - "+serialPort.BaudRate.ToString();
+		}
+
+		private void ms_sp_conect_Click(object sender, EventArgs e) {
+			try {
+				serialPort.Open();
+				ms_sp_conect.Enabled=false;
+				ms_sp_disconect.Enabled=true;
+			} catch(Exception ex) {
+				MessageBox.Show("Unable to conect to "+serialPort.PortName+" at "+serialPort.BaudRate+" kbps, select a valid port", "Conect Error");
+			}
+		}
+
+		private void ms_sp_disconect_Click(object sender, EventArgs e) {
+			try {
+				serialPort.Close();
+				ms_sp_conect.Enabled=true;
+				ms_sp_disconect.Enabled=false;
+			} catch(Exception ex) {
+				MessageBox.Show("Unable to disconect this serial port", "Disconect Error");
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			if(serialPort.IsOpen) serialPort.Close();
 		}
 	}
 }
